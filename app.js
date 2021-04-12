02c073d749e5bf732de2ecd334e8684201d06009
NITHISHKADAM-HUB <80144452+NITHISHKADAM-HUB@users.noreply.github.com>
2021-04-12 22:27:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const session = require('express-session')
+const methodOverride = require('method-override')
+var nodemailer = require('nodemailer');
+
+
+
+const initializePassport = require('./passportConfig')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+const users = []
+
+const app = express()
+app.set('view-engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, 'views')))
+
+app.use(session({
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+})
+
+app.post('/send', async (req, res) => {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'nithishkadam@gmail.com',
+            pass: 'leukocrepe1@'
+        }
+    });
+
+    var mailOptions = {
+        from: 'nithishkadam@gmail.com',
+        to: `${req.body.toEmail}`,
+        subject: `${req.body.subject}`,
+        text: `${req.body.message}`
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    res.redirect(req.get('referer'));
+})
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+app.listen(3001, () => {
+    console.log('Serving on port 3001')
+})
